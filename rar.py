c81bab6e4fd15775312887e12d4ee23af27c937c
Daniel Skowroński <daniel@dsinf.net>
2019-04-02 14:33:49 +0000

    tag filtering support, override access tokens via cli args

--- rar.py
+++ rar.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python
-import yaml,pocket,random
+import yaml,json,urllib,pocket,random,sys
 from pocket import Pocket
 from wsgiref.simple_server import make_server
-from tg import MinimalApplicationConfigurator,FullStackApplicationConfigurator,expose,TGController,session
+from tg import MinimalApplicationConfigurator,FullStackApplicationConfigurator,expose,TGController,session,request
 from tg.configurator.components.session import SessionConfigurationComponent
 
+if len(sys.argv)==3:
+	_master_rt=sys.argv[1]
+	_master_at=sys.argv[2]
+	_master_tokens=True
+else:
+	_master_tokens=False
+
 def getSessionItemOrEmpty(name):
+	if _master_tokens and name=='request_token':
+		return _master_rt
+	if _master_tokens and name=='access_token':
+		return _master_at
+
 	try:
 		x=session[name]
 	except:
@@ -15,7 +27,6 @@ def getSessionItemOrEmpty(name):
 class RootController(TGController):
 	@expose(content_type='text/html')
 	def index(self):
-		session['a']='a'
 		rt=getSessionItemOrEmpty('request_token')
 		at=getSessionItemOrEmpty('access_token')
 
@@ -35,9 +46,11 @@ class RootController(TGController):
 
 		html+='<h1>Pocket.RAR</h1>'
 		html+='<li><a href=/random style="font-size:72px">get random article</a></li>'
-		html+='<li><a href=https://github.com/danielskowronski/pocket.rar>github</a></li>'
+		html+='<li><a href=/tags>manage tags</a></li>'
 		html+='<li><a href=/login>auth to Pocket</a></li>'
 		html+='<li><a href=/logout>delete tokens from browser</a></li>'
+		html+='<br />'
+		html+='<li><a href=https://github.com/danielskowronski/pocket.rar>github</a></li>'
 
 		html+='</body>'
 
@@ -47,15 +60,59 @@ class RootController(TGController):
 	def random(self):
 		at=getSessionItemOrEmpty('access_token')
 		if at=="":
-			return '<script>window.location.replace("/index")</script>'
+			return '<script>window.location.replace("/login")</script>'
 
-		pocket_instance = pocket.Pocket(rar_config['consumer_key'], session['access_token'])
-		resp=pocket_instance.get(state='unread',tag='_untagged_') ##TODO: support tag
+		try:
+			tags=json.loads(urllib.unquote(request.cookies.get('tags')))
+		except:
+			tags='_untagged_'
+		pocket_instance = pocket.Pocket(rar_config['consumer_key'], at)
+		resp=pocket_instance.get(state='unread',tag=tags)
 		articles=resp[0]['list']
 		cnt=len(articles)
 		trgt=random.randint(0, cnt)
 
 		return '<script>window.location.href="https://getpocket.com/a/read/'+articles[articles.keys()[trgt]]['item_id']+'"</script>'
+
+	@expose(content_type='text/html')
+	def tags(self,*args):
+		at=getSessionItemOrEmpty('access_token')
+		if at=="":
+			return '<script>window.location.replace("/login")</script>'
+
+		pocket_instance = pocket.Pocket(rar_config['consumer_key'], at)
+		resp=pocket_instance.get(state='unread',detailType='complete')
+		articles=resp[0]['list']
+		ai=articles.iteritems()
+
+		tags=set()
+		for k,a in ai:
+			try:
+				for t in a['tags'].items():
+					tags.add(t[0])
+			except:
+				pass
+		html='<body><h1>tags manager</h1> \n'
+		html+='<script src=https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js></script> \n'
+		html+='<script src=https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.js></script> \n'
+		html+='<script>var tags;$(function() { readCookie() }); function readCookie(){ \n'
+		html+='  if (!$.cookie("tags")) $.cookie("tags", "[]") \n'
+		html+='  tags=JSON.parse($.cookie("tags")) \n'
+		html+='  $("li").each(function(){ \n'
+		html+='    t=$(this).html().split(" <")[0]; \n'
+		html+='    if (tags.includes(t)) $(this).css("color","green"); else $(this).css("color","red"); \n'
+		html+='  }) \n'
+		html+='} \n'
+		html+='function arrayRemove(arr, value) {return arr.filter(function(ele){return ele != value;});} \n'
+		html+='function toggle(tag){ \n'
+		html+='  if (!tags.includes(tag)) tags.push(tag); else tags=arrayRemove(tags,tag); \n'
+		html+='  $.cookie("tags", JSON.stringify(tags)); readCookie(); \n'
+		html+='}</script> \n'
+		for t in tags:
+			html+='<li>'+t+' <button onClick="toggle(\''+t+'\')">toggle</button></li> \n'
+		html+='<li>__untagged__ <button onClick="toggle(\''+t+'\')">toggle</button></li> \n'
+
+		return html
 
 	@expose(content_type='text/html')
 	def callback(self,*args):
